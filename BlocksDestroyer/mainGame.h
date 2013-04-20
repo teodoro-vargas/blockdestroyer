68c952c1670d6ba16b00ceef3f0840fe56bcd314
Teodoro Vargas Cortes <tevaco19@gmail.com>
2013-04-20 15:36:00 -0500

    Animacion para ver los puntos ganados

--- BlocksDestroyer/mainGame.h
+++ BlocksDestroyer/mainGame.h
@@ -9,7 +9,9 @@
 #import <Foundation/Foundation.h>
 #import <GameKit/GameKit.h>
 #import "cocos2d.h"
+#import <string.h>
 
+#define GAME_DURATION 90
 #define kMinimumGestureLength 10
 #define kMaximumVariance 5
 #define ROWS 6
@@ -19,14 +21,19 @@
 #define OFFSET 35
 #define DIST_CENTERS 65
 
+#define TO_DELETED 0
+#define EMPTY -1
+
 @interface mainGame : CCLayer<GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
 {
     CCLabelTTF *points_label;
     CCLabelTTF *timer_label;
     CCSprite *blocks_game[ROWS][COLUMNS];
     int indexGameBlocks[ROWS][COLUMNS];
+    BOOL processed[ROWS][COLUMNS];
     CGPoint gameCenters[ROWS][COLUMNS];
 }
+
 @property int pointsCounter;
 @property int timerCounter;
 @property CGPoint firstTouch;
@@ -49,6 +56,12 @@
 - (void)ccTouchEnded:(UITouch *)touch withEvent:(UIEvent *)event;
 - (int)nearColumn:(CGPoint)location forRow:(int)row;
 - (int)nearRow:(CGPoint)location forColumn:(int)column;
+- (void)getPoints;
+- (void)markToDelete:(int)index posX:(int)x posY:(int)y;
+- (int)checkEqualIndex:(int)index posX:(int)x posY:(int)y;
+- (void)destroyAdjacents;
+- (void)fillNewSprites;
+
 - (void)debug;
 
 @end
